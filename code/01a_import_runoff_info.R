96f5b673e702eb5ff6714434255f719348cbe868
yourname <martinroe13@gmail.com>
2020-04-07 16:53:21 +0200

    commit

--- code/01a_import_runoff_info.R
+++ code/01a_import_runoff_info.R
@@ -1,1 +1,14 @@
 library(data.table)
+
+list.files('data/raw')
+library(data.table)
+runoff_stations <- fread('./data/raw/runoff_stations.csv')
+head(runoff_stations)
+str(runoff_stations)
+runoff_stations$station
+runoff_stations[, sname := factor(abbreviate(station))]
+runoff_stations[, id := factor(id)]
+runoff_stations[, lat := round(lat, 3)]
+runoff_stations[, lon := round(lon, 3)]
+runoff_stations[, altitude := round(altitude, 0)]
+saveRDS(runoff_stations, './data/runoff_stations.rds')
---
+++ code/01b_import_runoff_info.R
@@ -0,0 +1,3 @@
+rm(list = ls())
+library(data.table)
+fread('./data/raw/runoff_day/6335020_Q_Day.Cmd.txt')
